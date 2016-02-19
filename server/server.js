d515c8c2072efdb05a01aabd6dcc628812b62952
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-02-18 22:30:04 -0800

    Implement database

--- server/server.js
+++ server/server.js
@@ -3,7 +3,23 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+
 mongoose.connect('mongodb://localhost/fizzbuzz');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error'));
+db.once('open', function() {
+  console.log('mongo db is open');
+})
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.post('/', function(req, res) {
+  res.sendStatus = 200;
+});
 
 app.listen(8000);
 
