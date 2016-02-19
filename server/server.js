1c69a4db7c461cbf156ba4ed4db0fb8358e66853
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-02-18 21:49:43 -0800

    Refactor to use Express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
-var http = require('http');
+var express = require('express');
+var mongoose = require('mongoose');
 
-http.createServer(function(req, res) {
-  res.end('Hello World');
-}).listen(8000, '127.0.0.1');
+var app = express();
+
+mongoose.connect('mongodb://localhost/fizzbuzz');
+
+app.listen(8000);
+
+module.exports = app;
